c4a511c1a32b9494510bbac9c29a31898e1b0a9e
Akhouri Vishal Sinha <vakhouri@gmail.com>
2020-02-26 16:42:58 +1100

    update packages: gulp, gulp-sass to v4

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function(){
-    gulp.src('styles/sass/*.scss')
+    return gulp.src('styles/sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('styles/css/'));
 });
 
 gulp.task('default', function() {
-    gulp.watch('styles/sass/*.scss',['styles']);
+    gulp.watch('styles/sass/*.scss', gulp.series(['styles']));
 });
